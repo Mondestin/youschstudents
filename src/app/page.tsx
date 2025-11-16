6dc299df14c8d716502f5850abf2130df78007cf
Mondestin Myssie <sydneymondestin@gmail.com>
2025-11-16 01:24:27 +0100

    remove ico to use the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  // Redirect to timetable (emploi du temps)
-  redirect('/dashboard/etudiants/emploi-du-temps');
+  // Redirect to login page
+  redirect('/login');
 }
